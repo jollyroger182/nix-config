4265badfb6a0d5ac0a094803198c32cd92ba72d1
jollyroger182 <hello@jollyy.dev>
2026-09-06 23:39:16 -0400

    manage user & add shell

--- configuration.nix
+++ configuration.nix
@@ -22,13 +22,19 @@
 
   system.primaryUser = "jolly";
 
+  users.knownUsers = [ "jolly" ];
+
   users.users.jolly = {
+    uid = 501;
     name = "jolly";
     description = "Jolly";
     home = "/Users/jolly";
     shell = pkgs.bashInteractive;
     isHidden = false;
   };
+
+  # Add the Nix bash to /etc/shells so it is a permitted login shell.
+  environment.shells = [ pkgs.bashInteractive ];
 
   # Enable alternative shell support in nix-darwin.
   # programs.fish.enable = true;
