6e383ba8e91d33241936b9dde33fb18a4afa76fd
jollyroger182 <hello@jollyy.dev>
2026-09-07 16:41:28 -0400

    ps1!

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -40,15 +40,14 @@
 
       __nix_ps1() { [ -n "$IN_NIX_SHELL" ] && printf '(nix) '; }
 
-      # NixOS's default prompt, but pink instead of green (still red under root).
+      # leading space to cover up the % mark when on new line
       if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
         PROMPT_COLOR="1;31m"
         ((UID)) && PROMPT_COLOR="1;38;5;213m"
         if [ -n "$INSIDE_EMACS" ]; then
-          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-          PS1="\n\[\033[$PROMPT_COLOR\]\$(__nix_ps1)[\u@\h:\w]\\$\[\033[0m\] "
+          PS1=" \n\[\033[$PROMPT_COLOR\]\$(__nix_ps1)[\u@\h:\w]\\$\[\033[0m\] "
         else
-          PS1="\n\[\033[$PROMPT_COLOR\]\$(__nix_ps1)[\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+          PS1=" \n\[\033[$PROMPT_COLOR\]\$(__nix_ps1)[\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
         fi
         if test "$TERM" = "xterm"; then
           PS1="\[\033]2;\h:\u:\w\007\]$PS1"
