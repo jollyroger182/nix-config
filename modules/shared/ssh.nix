a20fd32ce9b0006949003b86875e487d89e3010a
jollyroger182 <hello@jollyy.dev>
2026-09-09 00:40:23 -0400

    add ssh keys

--- modules/shared/ssh.nix
+++ modules/shared/ssh.nix
@@ -8,4 +8,11 @@
       PermitRootLogin = "prohibit-password";
     };
   };
+
+  users.users.jolly = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINSwBwIadrTlBexzP/o9TPdt7k/gU69Bye5FkfMj86aZ jolly@Nico"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmX/cNjOgEpsqwxB6XEBoA4IqonAjmPf8dS+zzoIk5i jolly@mira"
+    ];
+  };
 }
