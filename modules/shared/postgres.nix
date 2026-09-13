8afd9bed9cbbc85c7a5d9eea359eb85cc5e5bf6a
jollyroger182 <hello@jollyy.dev>
2026-09-13 00:02:05 -0400

    passwordless postgres from localhost

--- modules/shared/postgres.nix
+++ modules/shared/postgres.nix
@@ -16,5 +16,10 @@
         ensureClauses.superuser = true;
       }
     ];
+
+    authentication = ''
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128      trust
+    '';
   };
 }
